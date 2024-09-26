84f26fbedde4dd6831b3972ac9f9ea031f180441
anouar garbaa <g.anouar@yahoo.com>
2024-09-26 14:42:46 +0100

    add the data to list using apiSlice

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/todos",
+    }),
+  }),
+});
+
+export const { useGetTodosQuery } = apiSlice;
